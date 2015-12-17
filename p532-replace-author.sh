fa8b5ced7a9e871494eac496d9e8426a0583c2e4
Ankit Sadana <asadana@indiana.edu>
2015-12-17 17:47:47 -0500

    Added basic script

---
+++ p532-replace-author.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Git-Replace-Author
+# P532 - Sub-project
+
+# Made By: Ankit Sadana
+# Username: asadana
+# Made On: 12/17/2015
+
+# Last Edited By: Ankit Sadana
+# Username: asadana
+# Last Edited On: 12/17/2015
+
+# Source : git@github.com:asadana/Git-Replace-Author.git
+
+# ============================================================
+
+# Description goes here
+
+# ============================================================
+
